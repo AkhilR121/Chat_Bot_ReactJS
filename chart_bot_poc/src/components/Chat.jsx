a5cca942c22d6887f2bc9ed91c1d1d50a5f85caa
Akhil Vinnakota <akhil.vinnakota@nrg.com>
2026-01-09 12:28:44 +0530

    Add customer and site data JSON files; refactor App component to use ChatComponent

---
+++ chart_bot_poc/src/components/Chat.jsx
@@ -0,0 +1,10 @@
+function ChatComponent() {
+    return (
+        <div>
+            <h2>Chat Component</h2>
+            <p>This is where the chat interface will be implemented.</p>
+        </div>
+    );
+}
+
+export default ChatComponent;
