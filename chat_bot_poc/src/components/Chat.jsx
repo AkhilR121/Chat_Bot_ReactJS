77e15e745c5b757ddd72c41651efa0adecf2e79c
Akhil Vinnakota <akhil.vinnakota@nrg.com>
2026-01-09 16:58:17 +0530

    feat: add Tailwind CSS integration and update dependencies

--- chat_bot_poc/src/components/Chat.jsx
+++ chat_bot_poc/src/components/Chat.jsx
@@ -2,19 +2,18 @@ import { useQuery } from "@tanstack/react-query";
 import { getCustomers } from "../services/getData";
 
 function ChatComponent() {
-    const data = useQuery({
-        queryKey: ["customer-data"],
-        queryFn: () => getCustomers(),
-        initialData: () => [],
-    });
-console.log(data.data);
+  const data = useQuery({
+    queryKey: ["customer-data"],
+    queryFn: () => getCustomers(),
+    initialData: () => [],
+  });
 
-    return (
-        <div>
-            <h2>Chat Component</h2>
-            <p>This is where the chat interface will be implemented.</p>
-        </div>
-    );
+  return (
+    <div className="border-2 border-blue-500 p-4 rounded-md">
+      <h2>Chat Component</h2>
+      <p>This is where the chat interface will be implemented.</p>
+    </div>
+  );
 }
 
 export default ChatComponent;
