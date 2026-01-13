01c64e954be0230243f8db2354c49cf20dd94efd
Akhil Vinnakota <akhil.vinnakota@nrg.com>
2026-01-13 15:12:47 +0530

    improved chevron toggle logic

--- chat_bot_poc/src/components/Chat.jsx
+++ chat_bot_poc/src/components/Chat.jsx
@@ -3,17 +3,35 @@ import { getCustomers } from "../services/getData";
 import { useState } from "react";
 
 function ChatComponent() {
+  const [expanded, setExpanded] = useState({});
   const { data: customers = [] } = useQuery({
     queryKey: ["customer-data"],
     queryFn: getCustomers,
   });
+
+
+  const handleToggle = (path) => {
+    setExpanded((prev) => ({
+      ...prev,
+      [path]: !prev[path],
+    }));
+  };
 
   return (
     <>
       {customers.map((customer, i) => (
         <div key={i} className="flex flex-col text-left w-xl">
           {Object.entries(customer).map(([key, value]) => (
-            <KeyValueCard key={key} keyLabel={key} value={value} />
+            <>
+            <KeyValueCard
+              key={key}
+              keyLabel={key}
+              value={value}
+              expanded={expanded}
+              onToggle={handleToggle}
+              path={key}
+            />
+            </>
           ))}
         </div>
       ))}
@@ -21,10 +39,7 @@ function ChatComponent() {
   );
 }
 
-function KeyValueCard({ keyLabel, value }) {
-  const [toggle, setToggle] = useState(false);
-  const [nestedToggle, setNestedObjToggle] = useState(false);
-
+function KeyValueCard({ keyLabel, value, expanded, onToggle, path }) {
   const isObject = value && typeof value === "object" && !Array.isArray(value);
 
   if (!isObject) {
@@ -39,44 +54,67 @@ function KeyValueCard({ keyLabel, value }) {
 
   return (
     <div className="border-2 border-blue-500 p-4 m-4 rounded-md">
+      {path}
       <div className="flex gap-3">
         <h3 className="font-semibold mb-2">{keyLabel}</h3>
         {/* If there is nested object: chevron will render conditionally */}
-        <span
-          className={`fa ${
-            toggle ? "fa-angle-up" : "fa-angle-down"
-          } cursor-pointer text-4xl`}
-          style={{ fontSize: "26px" }}
-          onClick={() => setToggle(!toggle)}
-        ></span>
+        <ChevronIcon toggle={!!expanded[path]} onToggle={() => onToggle(path)} />
       </div>
-      {toggle && renderNestedValue(value, nestedToggle, setNestedObjToggle)}
+      {!!expanded[path] && renderNestedValue(value, expanded, onToggle, path)}
     </div>
   );
 }
 
-function renderNestedValue(value, nestedToggle, setNestedObjToggle) {
-
+function renderNestedValue(value, expanded, onToggle, parentPath) {
   if (value && typeof value === "object" && !Array.isArray(value)) {
     return (
       <ul className="ml-4 list-disc">
-        {Object.entries(value).map(([childKey, childValue]) => (
-          <li key={childKey}>
-            <span className="font-semibold">{childKey}:</span>
-            {childValue &&
+        {Object.entries(value).map(([childKey, childValue]) => {
+          const childPath = `${parentPath}.${childKey}`;
+          const isChildObject =
+            childValue &&
             typeof childValue === "object" &&
-            !Array.isArray(childValue)
-              ? renderNestedValue(childValue)
-              : childValue == null
-              ? "---"
-              : String(childValue)}
-          </li>
-        ))}
+            !Array.isArray(childValue);
+          const isOpen = !!expanded[childPath];
+
+          return (
+            <li key={childPath}>
+              <span className="font-semibold">
+                {childKey}
+                {isChildObject && (
+                  <ChevronIcon
+                    toggle={isOpen}
+                    onToggle={() => onToggle(childPath)}
+                  />
+                )}
+              </span>
+              {isChildObject && isOpen
+                ? renderNestedValue(childValue, expanded, onToggle, childPath)
+                : !isChildObject
+                ? childValue == null
+                  ? "---"
+                  : String(childValue)
+                : null}
+            </li>
+          );
+        })}
       </ul>
     );
   }
 
   return value == null ? "---" : String(value);
+}
+
+function ChevronIcon({ toggle, onToggle }) {
+  return (
+    <span
+      className={`fa ${
+        toggle ? "fa-angle-up" : "fa-angle-down"
+      } cursor-pointer text-4xl`}
+      style={{ fontSize: "26px" }}
+      onClick={onToggle}
+    ></span>
+  );
 }
 
 export default ChatComponent;
